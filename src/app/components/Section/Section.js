e3fc0bd91756c9e4f1f38efdd5e974ff4782c6ad
Benedikt D Valdez <benediktvaldez@gmail.com>
2018-01-17 01:31:00 +0000

    fix(Section): only update visible sections

--- src/app/components/Section/Section.js
+++ src/app/components/Section/Section.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import * as sections from './sections'
 import './Section.scss'
 
-const Section = ({ type, children, ...props }) => {
-  let sectionEl = null
-
-  if (sections && sections[type]) {
-    sectionEl = sections[type]
+class Section extends Component {
+  shouldComponentUpdate(nextProps) {
+    this.rect = this.section.getBoundingClientRect()
+    return (
+      this.rect.top < this.rect.height * 1 &&
+      this.rect.bottom > this.rect.height * 0.25
+    )
   }
 
-  return (
-    <section>
-      {React.createElement(
-        sectionEl || 'p',
-        props,
-        sectionEl ? children : `No section with type ${type}`
-      )}
-    </section>
-  )
+  render() {
+    this.rect = this.section ? this.section.getBoundingClientRect() : {}
+    const { type, children, ...props } = this.props
+    let sectionEl = null
+
+    if (sections && sections[type]) {
+      sectionEl = sections[type]
+    }
+
+    return (
+      <section
+        className="row"
+        ref={section => {
+          this.section = section
+        }}
+      >
+        {React.createElement(
+          sectionEl || 'p',
+          sectionEl ? { type, rect: this.rect, ...props } : {},
+          sectionEl ? children : `No section with type ${type}`
+        )}
+      </section>
+    )
+  }
 }
 
 export default Section
