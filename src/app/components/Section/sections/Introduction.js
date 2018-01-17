819e704bcf774a71437dc05b0d84e8cd6a1b9519
Benedikt D Valdez <benediktvaldez@gmail.com>
2018-01-17 01:31:49 +0000

    feat(Introduction): set up section

--- src/app/components/Section/sections/Introduction.js
+++ src/app/components/Section/sections/Introduction.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default ({ children }) => {
-  return (
-    <div>
-      <h2>Introduction</h2>
-      {children}
-    </div>
-  )
+import Avatar from 'app/components/Avatar'
+
+import './Introduction.scss'
+
+class Introduction extends Component {
+  render() {
+    const { rect, ...props } = this.props
+    const value = -1 + rect.top / rect.height || 0
+    const per = value < -1 ? -1 : value
+
+    const active = rect.top < rect.height * 0.5
+
+    return [
+      <div
+        key="content"
+        className={`content introduction col-12 col-md-9 offset-md-1 ${
+          active ? 'active' : 'inactive'
+        }`}
+      >
+        <p className="large">
+          I'm Benedikt Daníel Valdez Stefánsson; father, husband, creator,
+          developer, musician, photographer.
+        </p>
+      </div>,
+      <Avatar
+        key="avatar"
+        circle="blue"
+        image="blue"
+        className={`Avatar--introduction ${active ? 'active' : 'inactive'}`}
+        imageTransform={`translate3d(-${213}px, ${357 * per}px, 0)`}
+      />
+    ]
+  }
 }
+
+export default Introduction
